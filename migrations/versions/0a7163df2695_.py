2500394d5a42687d32c685bd9f3297a4cb58823b
Fredrik Kindström <fredrikkindstrom@hotmail.com>
2020-05-01 13:23:07 +0200

    Finished Chapter 6: Profile Page and Avatars

---
+++ migrations/versions/0a7163df2695_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0a7163df2695
+Revises: 253a1e88db69, 780739b227a7
+Create Date: 2020-04-30 21:46:33.120437
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0a7163df2695'
+down_revision = ('253a1e88db69', '780739b227a7')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
